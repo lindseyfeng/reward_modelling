d517c390c847535ba8c6afd30649458b53dfac0e
Lindsey Feng <lindseyf@Lindseys-MacBook-Air.local>
2024-02-16 10:17:17 -0500

    upsh

--- reward_trainer.py
+++ reward_trainer.py
@@ -36,6 +36,7 @@ if is_peft_available():
 BETA = 0.7
 TEMPERATURE = 1/1.2
 EPOCH = 2
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 class IterativeRewardTrainer(Trainer):
     r"""
@@ -306,7 +307,7 @@ class IterativeRewardTrainer(Trainer):
         for batch in data_loader:
             # Create a tensor of ones with size equal to the batch size
             # Since the batch size is 4, we create a tensor of shape (4,)
-            labels = torch.ones((4, 512, 32000), dtype=torch.long)
+            labels = torch.ones((4, 1, 1), dtype=torch.long).to(device)
             
             # If your DataLoader yields dictionaries, you might want to add labels to the batch directly
             # Ensure you're not overwriting anything important if you choose to do this
