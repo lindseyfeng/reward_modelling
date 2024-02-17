80a72845fcadc4990e79e985b989274e29674bfb
Lindsey Feng <lindseyf@ipsec-10-2-192-230.vpn.gatech.edu>
2024-02-17 02:21:46 -0500

    upsh

--- reward_trainer.py
+++ reward_trainer.py
@@ -332,9 +332,11 @@ class IterativeRewardTrainer(Trainer):
         Perform a training step, including updating labels based on model predictions.
         """
         model.train()
+        print(inputs)
         inputs = self._prepare_inputs(inputs)
-
+        print(inputs)
         # Compute loss with updated labels
+
         if 'label' not in inputs:
             inputs['label'] = fetch_labels_for_inputs()
             print("labels not found")
@@ -356,31 +358,32 @@ class IterativeRewardTrainer(Trainer):
 
     def _remove_unused_columns(self, dataset: "datasets.Dataset", description: Optional[str] = None):
         print("yes1")
-        if not self.args.remove_unused_columns:
-            return dataset
-        self._set_signature_columns_if_needed()
-        signature_columns = self._signature_columns
-        signature_columns.add("label")
+        return dataset
+        # if not self.args.remove_unused_columns:
+        #     return dataset
+        # self._set_signature_columns_if_needed()
+        # signature_columns = self._signature_columns
+        # signature_columns.add("label")
 
-        ignored_columns = list(set(dataset.column_names) - set(signature_columns))
-        if len(ignored_columns) > 0:
-            dset_description = "" if description is None else f"in the {description} set"
-            logger.info(
-                f"The following columns {dset_description} don't have a corresponding argument in "
-                f"`{self.model.__class__.__name__}.forward` and have been ignored: {', '.join(ignored_columns)}."
-                f" If {', '.join(ignored_columns)} are not expected by `{self.model.__class__.__name__}.forward`, "
-                " you can safely ignore this message."
-            )
+        # ignored_columns = list(set(dataset.column_names) - set(signature_columns))
+        # if len(ignored_columns) > 0:
+        #     dset_description = "" if description is None else f"in the {description} set"
+        #     logger.info(
+        #         f"The following columns {dset_description} don't have a corresponding argument in "
+        #         f"`{self.model.__class__.__name__}.forward` and have been ignored: {', '.join(ignored_columns)}."
+        #         f" If {', '.join(ignored_columns)} are not expected by `{self.model.__class__.__name__}.forward`, "
+        #         " you can safely ignore this message."
+        #     )
 
-        columns = [k for k in signature_columns if k in dataset.column_names]
+        # columns = [k for k in signature_columns if k in dataset.column_names]
 
-        if version.parse(datasets.__version__) < version.parse("1.4.0"):
-            dataset.set_format(
-                type=dataset.format["type"], columns=columns, format_kwargs=dataset.format["format_kwargs"]
-            )
-            return dataset
-        else:
-            return dataset.remove_columns(ignored_columns)
+        # if version.parse(datasets.__version__) < version.parse("1.4.0"):
+        #     dataset.set_format(
+        #         type=dataset.format["type"], columns=columns, format_kwargs=dataset.format["format_kwargs"]
+        #     )
+        #     return dataset
+        # else:
+        #     return dataset.remove_columns(ignored_columns)
 
     
 
