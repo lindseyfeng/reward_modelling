e132d77ca1b34d391bc9318e0181866765c271b7
Lindsey Feng <lindseyf@ipsec-10-2-192-230.vpn.gatech.edu>
2024-02-17 00:28:21 -0500

    upsh

--- reward_modelling.py
+++ reward_modelling.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 
 PAD_TOKEN = '[PAD]'
 tokenizer = AutoTokenizer.from_pretrained("microsoft/deberta-v3-large")
-model = AutoModel.from_pretrained("microsoft/deberta-v3-large")
+model = AutoModelForCausalLM.from_pretrained("microsoft/deberta-v3-large")
 # Assuming `tokenizer` is your tokenizer instance
 if tokenizer.pad_token is None:
     tokenizer.pad_token = PAD_TOKEN
