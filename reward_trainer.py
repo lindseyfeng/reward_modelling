d64677a6fb5264ece20b2316ced58ebd74babd28
Lindsey Feng <lindseyf@ipsec-10-2-192-230.vpn.gatech.edu>
2024-02-16 23:05:09 -0500

    ppo

--- reward_trainer.py
+++ reward_trainer.py
@@ -365,7 +365,8 @@ class IterativeRewardTrainer(Trainer):
                 
                 loss = loss / gradient_accumulation_steps  # Adjust loss for accumulation
                 with torch.autograd.detect_anomaly():
-                    scaler.scale(loss).backward()  # Scale loss before backward
+                    # scaler.scale(loss).backward()  # Scale loss before backward
+                    loss.backward()
                 scaler.unscale_(self.optimizer)
                 torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1.0)
                 if (step + 1) % gradient_accumulation_steps == 0 or (step + 1) == len_data:
