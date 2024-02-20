3f8560ebdda78f161c234b1a2440618b3f4346fc
Lindsey Feng <lindseyf@ipsec-10-2-192-230.vpn.gatech.edu>
2024-02-20 10:42:02 -0500

    update

--- temperature_scaling.py
+++ temperature_scaling.py
@@ -47,11 +47,11 @@ def temperature_scale(logits, temperature):
 
 def set_temperature(valid_loader, model, temperature):
     nll_criterion = nn.CrossEntropyLoss().cuda()
-
-    logits_list = []
-    labels_list = []
+    ece_criterion = _ECELoss().cuda()
     with torch.no_grad():
         for inputs in valid_loader:
+            logits_list = []
+            labels_list = []
             # Stack and move to the correct device
             input_ids_chosen_tensor = torch.stack(inputs["input_ids_chosen"]).to(model.device).transpose(0, 1)
             attention_mask_chosen_tensor = torch.stack(inputs["attention_mask_chosen"]).to(model.device).transpose(0, 1)
@@ -79,7 +79,8 @@ def set_temperature(valid_loader, model, temperature):
             labels = torch.tensor(labels_list).cuda()
             # Calculate NLL and ECE before temperature scaling
             before_temperature_nll = nll_criterion(logits, labels).item()
-            print('Before temperature - NLL: %.3f' % (before_temperature_nll))
+            before_temperature_ece = ece_criterion(logits, labels).item()
+            print('Before temperature - NLL: %.3f ECE: %.3f' %  (before_temperature_nll, before_temperature_ece))
 
             # Optimize the temperature
             print(temperature.is_leaf) 
@@ -93,8 +94,9 @@ def set_temperature(valid_loader, model, temperature):
 
             # Calculate NLL after temperature scaling
             after_temperature_nll = nll_criterion(temperature_scale(logits, temperature), labels).item()
+            after_temperature_ece = ece_criterion(self.temperature_scale(logits), labels).item()
             print('Optimal temperature: %.3f' % temperature.item())
-            print('After temperature - NLL: %.3f' % (after_temperature_nll))
+            print('After temperature - NLL: %.3f ECE: %.3f' % (after_temperature_nll, after_temperature_ece))
 
 
 tokenizer = AutoTokenizer.from_pretrained("weqweasdas/hh_rlhf_rm_open_llama_3b")
@@ -122,5 +124,48 @@ print(valid_loader)
 set_temperature(valid_loader, model, temperature)
     
 
+class _ECELoss(nn.Module):
+    """
+    Calculates the Expected Calibration Error of a model.
+    (This isn't necessary for temperature scaling, just a cool metric).
 
+    The input to this loss is the logits of a model, NOT the softmax scores.
+
+    This divides the confidence outputs into equally-sized interval bins.
+    In each bin, we compute the confidence gap:
+
+    bin_gap = | avg_confidence_in_bin - accuracy_in_bin |
+
+    We then return a weighted average of the gaps, based on the number
+    of samples in each bin
+
+    See: Naeini, Mahdi Pakdaman, Gregory F. Cooper, and Milos Hauskrecht.
+    "Obtaining Well Calibrated Probabilities Using Bayesian Binning." AAAI.
+    2015.
+    """
+    def __init__(self, n_bins=15):
+        """
+        n_bins (int): number of confidence interval bins
+        """
+        super(_ECELoss, self).__init__()
+        bin_boundaries = torch.linspace(0, 1, n_bins + 1)
+        self.bin_lowers = bin_boundaries[:-1]
+        self.bin_uppers = bin_boundaries[1:]
+
+    def forward(self, logits, labels):
+        softmaxes = F.softmax(logits, dim=1)
+        confidences, predictions = torch.max(softmaxes, 1)
+        accuracies = predictions.eq(labels)
+
+        ece = torch.zeros(1, device=logits.device)
+        for bin_lower, bin_upper in zip(self.bin_lowers, self.bin_uppers):
+            # Calculated |confidence - accuracy| in each bin
+            in_bin = confidences.gt(bin_lower.item()) * confidences.le(bin_upper.item())
+            prop_in_bin = in_bin.float().mean()
+            if prop_in_bin.item() > 0:
+                accuracy_in_bin = accuracies[in_bin].float().mean()
+                avg_confidence_in_bin = confidences[in_bin].mean()
+                ece += torch.abs(avg_confidence_in_bin - accuracy_in_bin) * prop_in_bin
+
+        return ece
     
