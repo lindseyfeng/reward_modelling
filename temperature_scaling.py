d8d041fa50913a6e011a7b57ced546232b260a83
Lindsey Feng <lindseyf@lawn-143-215-125-228.lawn.gatech.edu>
2024-02-26 17:16:11 -0500

    update

--- temperature_scaling.py
+++ temperature_scaling.py
@@ -99,10 +99,11 @@ def set_temperature(valid_loader, model, temperature):
     nll_criterion = nn.BCELoss().cuda()
     ece_criterion = _ECELoss().cuda()
     with torch.no_grad():
+        logits_list = []
+        labels_list = []
         for inputs in valid_loader:
-            logits_list = []
-            labels_list = []
             # Stack and move to the correct device
+            print("k")
             input_ids_chosen_tensor = torch.stack(inputs["input_ids_chosen"]).to(model.device).transpose(0, 1)
             attention_mask_chosen_tensor = torch.stack(inputs["attention_mask_chosen"]).to(model.device).transpose(0, 1)
             input_ids_rejected_tensor = torch.stack(inputs["input_ids_rejected"]).to(model.device).transpose(0, 1)
@@ -122,37 +123,37 @@ def set_temperature(valid_loader, model, temperature):
             labels_list += [1] * bsz # Assuming binary labels, adjust as necessary
             print(logits_list)
             # Convert logits list to tensor and labels list to tensor
-            logits = torch.tensor(logits_list).cuda()
-            labels = torch.tensor(labels_list).cuda()
+        logits = torch.tensor(logits_list).cuda()
+        labels = torch.tensor(labels_list).cuda()
             # Calculate NLL and ECE before temperature scaling
-            before_temperature_nll = nll_criterion(logits, labels).item()
-            before_temperature_ece = ece_criterion(logits, labels).item()
-            print('Before temperature - NLL: %.3f ECE: %.3f' %  (before_temperature_nll, before_temperature_ece))
+        before_temperature_nll = nll_criterion(logits, labels).item()
+        before_temperature_ece = ece_criterion(logits, labels).item()
+        print('Before temperature - NLL: %.3f ECE: %.3f' %  (before_temperature_nll, before_temperature_ece))
 
             # Optimize the temperature
-            print(temperature.is_leaf) 
-            optimizer = optim.LBFGS([temperature], lr=0.001, max_iter=50)
-            def eval():
-                optimizer.zero_grad()
-                loss = nll_criterion(temperature_scale(logits, temperature), labels)
-                loss.backward()
-                return loss
-            optimizer.step(eval)
+        print(temperature.is_leaf) 
+        optimizer = optim.LBFGS([temperature], lr=0.001, max_iter=50)
+        def eval():
+            optimizer.zero_grad()
+            loss = nll_criterion(temperature_scale(logits, temperature), labels)
+            loss.backward()
+            return loss
+        optimizer.step(eval)
 
             # Calculate NLL after temperature scaling
-            after_temperature_nll = nll_criterion(temperature_scale(logits, temperature), labels).item()
-            after_temperature_ece = ece_criterion(temperature_scale(logits, temperature), labels).item()
-            print('Optimal temperature: %.3f' % temperature.item())
-            print('After temperature - NLL: %.3f ECE: %.3f' % (after_temperature_nll, after_temperature_ece))
+        after_temperature_nll = nll_criterion(temperature_scale(logits, temperature), labels).item()
+        after_temperature_ece = ece_criterion(temperature_scale(logits, temperature), labels).item()
+        print('Optimal temperature: %.3f' % temperature.item())
+        print('After temperature - NLL: %.3f ECE: %.3f' % (after_temperature_nll, after_temperature_ece))
 
 
 tokenizer = AutoTokenizer.from_pretrained("weqweasdas/hh_rlhf_rm_open_llama_3b")
 PAD_TOKEN = '[PAD]'
 if tokenizer.pad_token is None:
     tokenizer.pad_token = PAD_TOKEN
-model = AutoModelForSequenceClassification.from_pretrained("weqweasdas/hh_rlhf_rm_open_llama_3b").to(device).to(torch.bfloat16)
+model = AutoModelForSequenceClassification.from_pretrained("weqweasdas/hh_rlhf_rm_open_llama_3b").to(device)
 raw_datasets = load_dataset("Anthropic/hh-rlhf")["test"] #.shuffle(seed=42).select(range(2000))
-bsz = 1000
+bsz = 200
 raw_datasets = raw_datasets.map(
         preprocess_function,
         batched=True,
@@ -163,7 +164,7 @@ raw_datasets = raw_datasets.filter(
         and len(x["input_ids_rejected"]) <= 512
     )
 print(raw_datasets)
-temperature = nn.Parameter(torch.ones(1) * 1.5)
+temperature = nn.Parameter(torch.ones(1) * 1.5).requires_grad_().to(device)
 print(temperature.is_leaf) 
 valid_loader = torch.utils.data.DataLoader(raw_datasets, pin_memory=True, batch_size=bsz, collate_fn=custom_collate_fn)
 print(valid_loader)
