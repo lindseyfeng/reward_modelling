74f5db485fa6f8c861793f83674e3ad751e47fef
Lindsey Feng <lindseyf@Lindseys-MacBook-Air.local>
2024-02-27 12:27:26 -0500

    update

--- temperature_scaling.py
+++ temperature_scaling.py
@@ -106,10 +106,12 @@ def set_temperature(valid_loader, model, temperature):
 
             # Note: Corrected model input to use tensors instead of lists
             rewards_chosen = model(input_ids=input_ids_chosen_tensor, attention_mask=attention_mask_chosen_tensor, return_dict=True)["logits"]
+            print(rewards_chosen)
             # prob_pos_class = torch.sigmoid(rewards_chosen)
             # prob_neg_class = 1 - prob_pos_class
             # prob_chosen = torch.cat((prob_pos_class.unsqueeze(-1), prob_neg_class.unsqueeze(-1)), dim=-1)
             rewards_rejected = model(input_ids=input_ids_rejected_tensor, attention_mask=attention_mask_rejected_tensor, return_dict=True)["logits"]
+            print(rewards_rejected)
             # prob_pos_class = torch.sigmoid(rewards_rejected)
             # prob_neg_class = 1 - prob_pos_class
             # prob_reject = torch.cat((prob_pos_class.unsqueeze(-1), prob_neg_class.unsqueeze(-1)), dim=-1)
@@ -118,6 +120,7 @@ def set_temperature(valid_loader, model, temperature):
             # print(pos_logits)
             # neg_logits = -pos_logits
             # logits_list.append(torch.cat((pos_logits.unsqueeze(-1), neg_logits.unsqueeze(-1)), dim=-1))
+            logits_list.append(pos_logits)
             print(logits_list)
             # Convert logits list to tensor and labels list to tensor
         # llama3b
