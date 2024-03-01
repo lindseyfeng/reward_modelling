c2cecc409211c1cfc7ddd97b0907e10f1a7a00a2
Lindsey Feng <lindseyf@Lindseys-MacBook-Air.local>
2024-03-01 17:08:08 -0500

    update

--- temperature_scaling.py
+++ temperature_scaling.py
@@ -142,7 +142,7 @@ def set_temperature(valid_loader, model, temperature):
 
             # Optimize the temperature
         print(temperature.is_leaf) 
-        optimizer = optim.LBFGS([temperature], lr=0.01, max_iter=100)
+        optimizer = optim.LBFGS([temperature], lr=0.001, max_iter=100)
         def eval():
             optimizer.zero_grad()
             loss = nll_criterion(temperature_scale(logits, temperature), labels)
