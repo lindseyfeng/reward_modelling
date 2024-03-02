6a7363ac8541180c2b0a661c70ccca3cc197e752
Lindsey Feng <lindseyf@Lindseys-MacBook-Air.local>
2024-03-02 00:03:54 -0500

    update

--- temperature_scaling.py
+++ temperature_scaling.py
@@ -109,18 +109,11 @@ def set_temperature(valid_loader, model, temperature):
             # Note: Corrected model input to use tensors instead of lists
             rewards_chosen = model(input_ids=input_ids_chosen_tensor, attention_mask=attention_mask_chosen_tensor, return_dict=True)["logits"]
             print(rewards_chosen)
-            # prob_pos_class = torch.sigmoid(rewards_chosen)
-            # prob_neg_class = 1 - prob_pos_class
-            # prob_chosen = torch.cat((prob_pos_class.unsqueeze(-1), prob_neg_class.unsqueeze(-1)), dim=-1)
             rewards_rejected = model(input_ids=input_ids_rejected_tensor, attention_mask=attention_mask_rejected_tensor, return_dict=True)["logits"]
             print(rewards_rejected)
-            # prob_pos_class = torch.sigmoid(rewards_rejected)
-            # prob_neg_class = 1 - prob_pos_class
-            # prob_reject = torch.cat((prob_pos_class.unsqueeze(-1), prob_neg_class.unsqueeze(-1)), dim=-1)
             # Accumulate logits and labels
-            pos_logits = rewards_chosen - rewards_rejected
             # print(pos_logits)
-            logits_list.append(pos_logits) 
+            logits_list.append(rewards_chosen - rewards_rejected) 
             print(logits_list)
             # Convert logits list to tensor and labels list to tensor
         # llama3b
@@ -142,7 +135,7 @@ def set_temperature(valid_loader, model, temperature):
 
             # Optimize the temperature
         print(temperature.is_leaf) 
-        optimizer = optim.LBFGS([temperature], lr=0.001, max_iter=100)
+        optimizer = optim.LBFGS([temperature], lr=0.1, max_iter=100)
         def eval():
             optimizer.zero_grad()
             loss = nll_criterion(temperature_scale(logits, temperature), labels)
@@ -163,7 +156,7 @@ PAD_TOKEN = tokenizer.eos_token
 if tokenizer.pad_token is None:
     tokenizer.pad_token = PAD_TOKEN
 model = AutoModelForSequenceClassification.from_pretrained("./open_llama_3b_rlhf_rm__2e-05__last_checkpoint").to(device)
-raw_datasets = load_dataset("Dahoas/full-hh-rlhf")["test"]
+raw_datasets = load_dataset("Dahoas/full-hh-rlhf")["test"].select(range(40))
 bsz = 30
 raw_datasets = raw_datasets.map(
         preprocess_function,
