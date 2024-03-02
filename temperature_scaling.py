2a2bd039ec7bb1cdcc3a39d81c056ff661771675
Lindsey Feng <lindseyf@Lindseys-MacBook-Air.local>
2024-03-02 00:42:53 -0500

    update

--- temperature_scaling.py
+++ temperature_scaling.py
@@ -157,11 +157,11 @@ def set_temperature(valid_loader, model, temperature):
         print('After temperature - NLL: %.3f ECE: %.3f' % (after_temperature_nll, after_temperature_ece))
 
 
-tokenizer = AutoTokenizer.from_pretrained("openlm-research/open_llama_3b")
+tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neo-2.7B")
 PAD_TOKEN = '[PAD]'
 if tokenizer.pad_token is None:
     tokenizer.pad_token = PAD_TOKEN
-model = AutoModelForSequenceClassification.from_pretrained("./open_llama_3b_rlhf_rm__2e-05__last_checkpoint").to(device)
+model = AutoModelForSequenceClassification.from_pretrained("./gpt-neo-2.7B_rlhf_rm__2e-05/checkpoint-3000").to(device)
 raw_datasets = load_dataset("Dahoas/full-hh-rlhf")["test"]
 bsz = 30
 raw_datasets = raw_datasets.map(
