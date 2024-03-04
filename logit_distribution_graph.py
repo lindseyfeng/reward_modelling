ac5a3779604653d6a002db335399e63e80f68371
Lindsey Feng <lindseyf@Lindseys-MacBook-Air.local>
2024-03-04 13:20:30 -0500

    update

--- logit_distribution_graph.py
+++ logit_distribution_graph.py
@@ -2,7 +2,7 @@ import torch
 import json
 import matplotlib.pyplot as plt
 # Load logits from JSON
-file = 'logits_scores_._open_llama_3b_rlhf_rm__2e-05__temperature_last_checkpoint_train.json'
+file = 'logits_scores_._open_llama_3b_rlhf_rm_without_2e-05__last_checkpoint_train.json'
 with open(file, 'r') as file:
     data = json.load(file)
 logits_tensor = torch.tensor(data['logits'])
@@ -14,13 +14,13 @@ fig, axs = plt.subplots(1, 2, figsize=(12, 6))
 
 # Plotting the distribution of logits on the first subplot
 axs[0].hist(logits_tensor.numpy(), bins=10, color='red', alpha=0.7)
-axs[0].set_title('Distribution of Logits with temperature')
+axs[0].set_title('Distribution of Logits')
 axs[0].set_xlabel('Logits')
 axs[0].set_ylabel('Frequency')
 
 # Plotting the distribution of probabilities on the second subplot
 axs[1].hist(probabilities.numpy(), bins=10, color='blue', alpha=0.7)
-axs[1].set_title('Distribution of Probabilities with temperature')
+axs[1].set_title('Distribution of Probabilities')
 axs[1].set_xlabel('Probabilities')
 axs[1].set_ylabel('Frequency')
 
