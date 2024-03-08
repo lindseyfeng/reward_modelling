71db03c5f28e4f1be965a647956ca37af894c186
Lindsey Feng <lindseyf@Lindseys-MacBook-Air.local>
2024-03-08 02:15:18 -0500

    update

--- trl_bin_temp.py
+++ trl_bin_temp.py
@@ -36,7 +36,7 @@ class TemperatureRewardTrainer(RewardTrainer):
         inputs: Dict[str, Union[torch.Tensor, Any]],
         return_outputs=False,
     ) -> Union[torch.Tensor, Tuple[torch.Tensor, Dict[str, torch.Tensor]]]:
-        temperature_list = [1, 1, 1, 1, 1] #5BIN
+        temperature_list = [1.5070, 1.7932, 1.7546, 1.8977, 1.9648] #5BIN
         bin_boundaries = torch.linspace(0.5, 1, steps=num_bins + 1)
         bin_lowers = bin_boundaries[:-1]
         bin_uppers = bin_boundaries[1:]
