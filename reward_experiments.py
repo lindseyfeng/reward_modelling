a015c1c814b8a820dbd1f1089001b564768ccda3
Lindsey Feng <lindseyf@Lindseys-MacBook-Air.local>
2024-03-10 20:00:13 -0400

    update

--- reward_experiments.py
+++ reward_experiments.py
@@ -48,7 +48,7 @@ def preprocess_function(examples):
     return new_examples
 
 # Example usage
-pretrained_model_name_or_path = './open_llama_3b_rlhf_rm_iterative_temperature_2e-05_last_checkpoint' 
+pretrained_model_name_or_path = './open_llama_3b_rlhf_rm_bin_temperature5_2e-05/checkpoint-14000' 
 tokenizer = AutoTokenizer.from_pretrained("openlm-research/open_llama_3b")
 if tokenizer.pad_token is None:
     tokenizer.pad_token = tokenizer.eos_token
