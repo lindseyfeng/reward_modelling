4287507b78c79675235f1d65c4894dd4ec548f0b
Lindsey Feng <lindseyf@Lindseys-MacBook-Air.local>
2024-03-13 22:13:48 -0400

    update

--- reward_modelling.py
+++ reward_modelling.py
@@ -165,7 +165,7 @@ raw_datasets = raw_datasets.filter(
         lambda x: len(x["input_ids_chosen"]) <= reward_config.max_length
         and len(x["input_ids_rejected"]) <= reward_config.max_length
     )
-train_dataset = raw_datasets["train"].select(range(2))
+train_dataset = raw_datasets["train"].select(range(1))
 eval_dataset = raw_datasets["test"]
 
 # peft_config = LoraConfig(
