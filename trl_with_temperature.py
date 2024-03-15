fc85fb56f85978cabd9de4da1d20e86c8f4d1956
Lindsey Feng <lindseyf@Lindseys-MacBook-Air.local>
2024-03-15 17:20:56 -0400

    update

--- trl_with_temperature.py
+++ trl_with_temperature.py
@@ -87,7 +87,7 @@ class ScriptArguments:
         },
     )
     per_device_train_batch_size: Optional[int] = field(default=2)
-    per_device_eval_batch_size: Optional[int] = field(default=3)
+    per_device_eval_batch_size: Optional[int] = field(default=2)
     gradient_accumulation_steps: Optional[int] = field(default=4)
     learning_rate: Optional[float] = field(default=2e-5)
     weight_decay: Optional[float] = field(default=0.001)
