a09675cd009d43114075c2c40ba682a4c8d1e85a
Lindsey Feng <lindseyf@ipsec-10-2-192-203.vpn.gatech.edu>
2024-03-16 10:53:29 -0400

    update

--- temperature_scaling.py
+++ temperature_scaling.py
@@ -214,9 +214,9 @@ if __name__ == "__main__":
     tokenizer = AutoTokenizer.from_pretrained("openlm-research/open_llama_3b") #openlm-research/open_llama_3b
     if tokenizer.pad_token is None:
         tokenizer.pad_token = tokenizer.eos_token
-    model = AutoModelForSequenceClassification.from_pretrained("./open_llama_3b_rlhf_rm_temperature1.374_2e-05__temperature_last_checkpoint").to(device)
+    model = AutoModelForSequenceClassification.from_pretrained("./right_open_llama_3b_rlhf_rm_iterative_temperature_0.5_epoch_2e-05_last_checkpoint").to(device)
     raw_datasets = load_dataset("Dahoas/full-hh-rlhf")["test"]
-    bsz = 20
+    bsz = 10
     raw_datasets = raw_datasets.map(
             preprocess_function,
             batched=True,
