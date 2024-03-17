5b788a1cb2e29987573354db6392e31f41d62b3f
Lindsey Feng <lindseyf@Lindseys-MacBook-Air.local>
2024-03-16 23:06:57 -0400

    update

--- dpo.py
+++ dpo.py
@@ -55,7 +55,7 @@ class ScriptArguments:
     save_steps: Optional[int] = field(default=100, metadata={"help": "the saving frequency"})
     eval_steps: Optional[int] = field(default=100, metadata={"help": "the evaluation frequency"})
 
-    output_dir: Optional[str] = field(default="./dpo_llama7b_results", metadata={"help": "the output directory"})
+    output_dir: Optional[str] = field(default="./dpo_llama7b_temperature_results", metadata={"help": "the output directory"})
     log_freq: Optional[int] = field(default=1, metadata={"help": "the logging frequency"})
 
     # instrumentation
