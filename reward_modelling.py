2bbc8197f51f7a2986a6623051d87ea8d17ec569
Lindsey Feng <lindseyf@Lindseys-MacBook-Air.local>
2024-03-16 23:04:18 -0400

    update

--- reward_modelling.py
+++ reward_modelling.py
@@ -172,7 +172,7 @@ def preprocess_function(examples):
             updated_label = chosen_to_label[chosen]
         else:
             updated_label = 1
-        new_examples["label"].append(updated_label)
+        new_examples["label"].append(torch.tensor([[update_labe]]))
     return new_examples
 
     # Preprocess the dataset and filter out examples that are longer than args.max_length
