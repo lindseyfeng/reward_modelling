b78b0cb3d01d4be1669991d8e1ea45462f60ecce
Lindsey Feng <lindseyf@Lindseys-MacBook-Air.local>
2024-03-18 15:06:37 -0400

    update

--- reliability_diagram.py
+++ reliability_diagram.py
@@ -2,7 +2,7 @@ import torch
 import json
 import matplotlib.pyplot as plt
 # Load logits from JSON
-file = 'logits_scores_._right_open_llama_3b_rlhf_rm_logit_bin_temperature_2e-05__temperature_last_checkpoint_test.json'
+file = 'logits_scores_._hh_openllama3b_temp1_epoch1_final_checkpoint_test.json'
 with open(file, 'r') as file:
     data = json.load(file)
 logits_tensor = torch.tensor(data['logits'])
