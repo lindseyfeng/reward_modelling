cafae2b4c39bb8ce7c903529d8e6be6bd58ea5fe
Lindsey Feng <lindseyf@lawn-143-215-125-120.lawn.gatech.edu>
2024-03-21 19:23:10 -0400

    update

--- dpo_temperature_scaling.py
+++ dpo_temperature_scaling.py
@@ -38,6 +38,8 @@ class _ECELoss(nn.Module):
         softmaxes = F.softmax(logits, dim=1)
         confidences, predictions = torch.max(softmaxes, 1)
         accuracies = predictions.eq(labels)
+        print(confidences)
+        print(accuracies)
 
         ece = torch.zeros(1, device=logits.device)
         for bin_lower, bin_upper in zip(self.bin_lowers, self.bin_uppers):
@@ -72,16 +74,19 @@ def preprocess_function(examples):
             "attention_mask_chosen": [],
             "input_ids_rejected": [],
             "attention_mask_rejected": [],
+            "prompt_input_ids":[]
     }
     for prompt, chosen, rejected in zip(examples["prompt"], examples["chosen"], examples["rejected"]):
         chosen_str = prompt + " " + chosen
         rejected_str = prompt + " " + rejected
+        tokenized_prompt = tokenizer(prompt)
         tokenized_chosen = tokenizer(chosen_str, padding = "max_length", max_length = 512)
         tokenized_rejected = tokenizer(rejected_str, padding = "max_length", max_length = 512)
         new_examples["input_ids_chosen"].append(tokenized_chosen["input_ids"])
         new_examples["attention_mask_chosen"].append(tokenized_chosen["attention_mask"])
         new_examples["input_ids_rejected"].append(tokenized_rejected["input_ids"])
         new_examples["attention_mask_rejected"].append(tokenized_rejected["attention_mask"])
+        new_examples["prompt_input_ids"].append(tokenized_prompt["input_ids"])
 
     return new_examples
 
@@ -104,6 +109,7 @@ def set_temperature_trl(valid_loader, model, temperature):
 
             # Note: Corrected model input to use tensors instead of lists
             rewards_chosen = model(input_ids=input_ids_chosen_tensor, attention_mask=attention_mask_chosen_tensor, return_dict=True).logits
+
             # prob_pos_class = torch.sigmoid(rewards_chosen)
             # prob_neg_class = 1 - prob_pos_class
             # prob_chosen = torch.cat((prob_pos_class.unsqueeze(-1), prob_neg_class.unsqueeze(-1)), dim=-1)
@@ -148,6 +154,44 @@ def set_temperature_trl(valid_loader, model, temperature):
         print('After temperature - NLL: %.3f ECE: %.3f' % (after_temperature_nll, after_temperature_ece))
         return before_temperature_ece
 
+def get_logps( logits: torch.FloatTensor,
+        labels: torch.LongTensor,
+        average_log_prob: bool = False,
+        label_pad_token_id: int = -100,
+        is_encoder_decoder: bool = False,
+    ) -> torch.FloatTensor:
+        """Compute the log probabilities of the given labels under the given logits.
+
+        Args:
+            logits: Logits of the model (unnormalized). Shape: (batch_size, sequence_length, vocab_size)
+            labels: Labels for which to compute the log probabilities. Label tokens with a value of label_pad_token_id are ignored. Shape: (batch_size, sequence_length)
+            average_log_prob: If True, return the average log probability per (non-masked) token. Otherwise, return the sum of the log probabilities of the (non-masked) tokens.
+            label_pad_token_id: The label pad token id.
+            is_encoder_decoder: Whether the model is an encoder-decoder model.
+
+        Returns:
+            A tensor of shape (batch_size,) containing the average/sum log probabilities of the given labels under the given logits.
+        """
+        if logits.shape[:-1] != labels.shape:
+            raise ValueError("Logits (batch and sequence length dim) and labels must have the same shape.")
+
+        if not is_encoder_decoder:
+            labels = labels[:, 1:].clone()
+            logits = logits[:, :-1, :]
+        loss_mask = labels != label_pad_token_id
+
+        # dummy token; we'll ignore the losses on these tokens later
+        labels[labels == label_pad_token_id] = 0
+
+        per_token_logps = torch.gather(logits.log_softmax(-1), dim=2, index=labels.unsqueeze(2)).squeeze(2)
+
+        if average_log_prob:
+            return (per_token_logps * loss_mask).sum(-1) / loss_mask.sum(-1)
+        else:
+            return (per_token_logps * loss_mask).sum(-1)
+
+
+
 
 
 def set_temperature(valid_loader, model, temperature):
@@ -167,21 +211,35 @@ def set_temperature(valid_loader, model, temperature):
 
             # Note: Corrected model input to use tensors instead of lists
             rewards_chosen = model(input_ids=input_ids_chosen_tensor, attention_mask=attention_mask_chosen_tensor, return_dict=True).logits
-            # prob_pos_class = torch.sigmoid(rewards_chosen)
-            # prob_neg_class = 1 - prob_pos_class
-            # prob_chosen = torch.cat((prob_pos_class.unsqueeze(-1), prob_neg_class.unsqueeze(-1)), dim=-1)
+            ref_rewards_chosen = ref_file(input_ids=input_ids_chosen_tensor, attention_mask=attention_mask_chosen_tensor, return_dict=True).logits
+
             rewards_rejected = model(input_ids=input_ids_rejected_tensor, attention_mask=attention_mask_rejected_tensor, return_dict=True).logits
+            ref_rewards_rejected = ref_file(input_ids=input_ids_chosen_tensor, attention_mask=attention_mask_chosen_tensor, return_dict=True).logits
             # prob_pos_class = torch.sigmoid(rewards_rejected)
             # prob_neg_class = 1 - prob_pos_class
             # prob_reject = torch.cat((prob_pos_class.unsqueeze(-1), prob_neg_class.unsqueeze(-1)), dim=-1)
             # Accumulate logits and labels
-            pos_logits = rewards_chosen - rewards_rejected
+            chosen_label = chosen_sequence_tokens["input_ids"][:]
+            chosen_label[: len(inputs["prompt_input_ids"])] = [
+                self.label_pad_token_id
+            ] * len(inputs["prompt_input_ids"])
+            rejected_sequence_tokens["labels"] = rejected_sequence_tokens["input_ids"][:]
+            rejected_sequence_tokens["labels"][: len(inputs["prompt_input_ids"])] = [
+                self.label_pad_token_id
+            ] * len(inputs["prompt_input_ids"])
+
+            chosen_logprob = get_logps(rewards_chosen, chosen_label)
+            ref_chosen_logprob = get_logps(ref_rewards_chosen, chosen_label)
+            reject_logprob = get_logps(rewards_reject, reject_label)
+            ref_reject_logprob = get_logps(ref_rewards_reject, reject_label)
+
+            pos_logits = (chosen_logprob-reject_logprob)-(ref_chosen_logprob-ref_reject_logprob)
             neg_logits = -pos_logits
             logits_list.append(torch.cat((pos_logits.unsqueeze(-1), neg_logits.unsqueeze(-1)), dim=-1))
             # Convert logits list to tensor and labels list to tensor
         # llama3b
         logits = torch.cat(logits_list, dim=0).squeeze(1)  # This is your tensor from logits_list
-
+        print(logits)
         N, _ = logits.shape
         labels_list += [0] * N # Assuming binary labels, adjust as necessary
         labels = torch.tensor(labels_list).cuda()
@@ -210,14 +268,15 @@ def set_temperature(valid_loader, model, temperature):
         print('After temperature - NLL: %.3f ECE: %.3f' % (after_temperature_nll, after_temperature_ece))
         return before_temperature_ece
 
-
 if __name__ == "__main__":
-    file = "../llama/llama-2-7b"
-    print(file)
+    ref_file = "../llama/llama-2-7b"
+    model_file="dpo_llama7b_results/checkpoint-1000"
+    print(model_file)
     tokenizer = AutoTokenizer.from_pretrained(file) #openlm-research/open_llama_3b
     if tokenizer.pad_token is None:
         tokenizer.pad_token = tokenizer.eos_token
     model = AutoModelForCausalLM.from_pretrained(file).to(device)
+    ref_model= AutoModelForCausalLM.from_pretrained(ref_file).to(device)
     raw_datasets = load_dataset("Dahoas/full-hh-rlhf")["test"].select(range(10))
     bsz = 5
     raw_datasets = raw_datasets.map(
