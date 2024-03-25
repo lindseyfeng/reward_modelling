7a6755cf0c8743619f4f094f171960baa691ca18
Lindsey Feng <lindseyf@Lindseys-MacBook-Air.local>
2024-03-24 23:59:12 -0400

    update

--- dpo_temperature_scaling.py
+++ dpo_temperature_scaling.py
@@ -9,6 +9,7 @@ from dataclasses import dataclass, field
 from typing import Dict, Optional
 import json
 from trl import DPOTrainer
+import wandb
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -371,7 +372,7 @@ if __name__ == "__main__":
         evaluation_strategy="steps",
         eval_steps=script_args.eval_steps,
         output_dir=script_args.output_dir,
-        # report_to=script_args.report_to,
+        report_to=script_args.report_to,
         lr_scheduler_type=script_args.lr_scheduler_type,
         warmup_steps=script_args.warmup_steps,
         optim=script_args.optimizer_type,
