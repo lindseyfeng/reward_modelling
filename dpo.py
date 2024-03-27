5a3711ae7e138e74eb5de783274cc9ebf5ac34e3
Lindsey Feng <lindseyf@Lindseys-MacBook-Air.local>
2024-03-27 02:43:07 -0400

    update

--- dpo.py
+++ dpo.py
@@ -28,11 +28,8 @@ class ECEDP0Trainer(DPOTrainer):
     def evaluate(self, eval_dataset=None, ignore_keys=None, metric_key_prefix="eval"):
         # Check if it's time to update beta
         if self.eval_step_counter % self.beta_update_interval == 0:
-            eval_dataset = self.get_eval_dataloader(eval_dataset).dataset
-            eval_dataloader = DataLoader(eval_dataset, batch_size=16, collate_fn=self.data_collator)
-            print(eval_dataloader.dataset)  # Should not be None
-            assert eval_dataloader.dataset is not None, "The DataLoader's dataset is None!"
-            ece = set_temperature_trl(eval_dataloader, self.model, self.temperature)
+            batch = self.data_collator(eval_dataset)
+            ece = set_temperature_trl(batch, self.model, self.temperature)
             log_value = self.temperature.detach().cpu().item()
             wandb.log({'temperature_trajectory': self.beta})
             wandb.log({'ece': ece})
