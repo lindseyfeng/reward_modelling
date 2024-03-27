2393b0863184a90d6a6cb25c9d03a6cbff2ac230
Lindsey Feng <lindseyf@Lindseys-MacBook-Air.local>
2024-03-27 15:34:42 -0400

    update

--- dpo.py
+++ dpo.py
@@ -28,30 +28,33 @@ class ECEDP0Trainer(DPOTrainer):
     def evaluate(self, eval_dataset=None, ignore_keys=None, metric_key_prefix="eval"):
         # Check if it's time to update beta
         # with torch.autocast(device_type="cuda", dtype=torch.bfloat16):
-        if self.eval_step_counter % self.beta_update_interval == 0:
-            eval_dataloader = self.get_eval_dataloader(eval_dataset)
-            eval_dataloader = self.data_collator(eval_dataloader.dataset)
-            (
-                        policy_chosen_logps,
-                        policy_rejected_logps,
-                        policy_chosen_logits,
-                        policy_rejected_logits,
-            ) = self.concatenated_forward(self.model, eval_dataloader)
-            losses, chosen_rewards, rejected_rewards = self.dpo_loss(
-                policy_chosen_logps,
-                policy_rejected_logps,
-                eval_dataloader["reference_chosen_logps"],
-                eval_dataloader["reference_rejected_logps"],
-            )
-            print("print", chosen_rewards.tolist(), rejected_rewards.tolist())
-            ece = set_temperature(chosen_rewards.tolist(), rejected_rewards.tolist(), self.temperature, script_args.output_dir)
-            log_value = self.temperature.detach().cpu().item()
-            wandb.log({'temperature_trajectory': self.beta})
-            wandb.log({'ece': ece})
-        
+        model.eval() 
+        with torch.no_grad():  
 
-        # Increment the counter
-        self.eval_step_counter += 1
+            if self.eval_step_counter % self.beta_update_interval == 0:
+                eval_dataloader = self.get_eval_dataloader(eval_dataset)
+                eval_dataloader = self.data_collator(eval_dataloader.dataset)
+                (
+                            policy_chosen_logps,
+                            policy_rejected_logps,
+                            policy_chosen_logits,
+                            policy_rejected_logits,
+                ) = self.concatenated_forward(self.model, eval_dataloader)
+                losses, chosen_rewards, rejected_rewards = self.dpo_loss(
+                    policy_chosen_logps,
+                    policy_rejected_logps,
+                    eval_dataloader["reference_chosen_logps"],
+                    eval_dataloader["reference_rejected_logps"],
+                )
+                print("print", chosen_rewards.tolist(), rejected_rewards.tolist())
+                ece = set_temperature(chosen_rewards.tolist(), rejected_rewards.tolist(), self.temperature, script_args.output_dir)
+                log_value = self.temperature.detach().cpu().item()
+                wandb.log({'temperature_trajectory': self.beta})
+                wandb.log({'ece': ece})
+            
+
+            # Increment the counter
+            self.eval_step_counter += 1
         
         # Now call the original evaluate function
         return super().evaluate(eval_dataset, ignore_keys, metric_key_prefix)
