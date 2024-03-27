c99d6dd0446ba338f290d6313aaa59e0fa2c8ea4
Lindsey Feng <lindseyf@Lindseys-MacBook-Air.local>
2024-03-27 13:54:05 -0400

    update

--- dpo.py
+++ dpo.py
@@ -30,13 +30,14 @@ class ECEDP0Trainer(DPOTrainer):
         # with torch.autocast(device_type="cuda", dtype=torch.bfloat16):
         if self.eval_step_counter % self.beta_update_interval == 0:
             eval_dataloader = self.get_eval_dataloader(eval_dataset)
-                # eval_dataloader = self.data_collator(eval_dataset)
-                # (
-                #     policy_chosen_logps,
-                #     policy_rejected_logps,
-                #     policy_chosen_logits,
-                #     policy_rejected_logits,
-                # ) = self.concatenated_forward(model, eval_dataloader)
+                with torch.autocast(device_type="cuda", dtype=torch.bfloat16):
+                    eval_dataloader = self.data_collator(eval_dataset)
+                    (
+                        policy_chosen_logps,
+                        policy_rejected_logps,
+                        policy_chosen_logits,
+                        policy_rejected_logits,
+                    ) = self.concatenated_forward(model, eval_dataloader)
                 # losses, chosen_rewards, rejected_rewards = self.dpo_loss(
                 # policy_chosen_logps,
                 # policy_rejected_logps,
