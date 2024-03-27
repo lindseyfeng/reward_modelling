24dfdf3a49db984e62b4a965fb41fa3d211d2a71
Lindsey Feng <lindseyf@Lindseys-MacBook-Air.local>
2024-03-27 00:40:54 -0400

    update

--- dpo.py
+++ dpo.py
@@ -29,10 +29,7 @@ class ECEDP0Trainer(DPOTrainer):
         # Check if it's time to update beta
         if self.eval_step_counter % self.beta_update_interval == 0:
             data_collator = DPODataCollatorWithPadding(padding_value=0)
-
-        # Initialize DataLoader with the custom collate_fn
-        eval_dataloader = DataLoader(eval_dataset, batch_size=32, collate_fn=data_collator)
-        
+            eval_dataloader = DataLoader(eval_dataset, batch_size=32, collate_fn=data_collator)
             ece = set_temperature_trl(eval_dataloader, self.model, self.temperature, self.tokenizer.pad_token)
             log_value = self.temperature.detach().cpu().item()
             wandb.log({'temperature_trajectory': self.beta})
